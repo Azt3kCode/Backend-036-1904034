f8a900e8472683955639e1b060ff8559c07415c5
Azt3kCode <62321351+Azt3kCode@users.noreply.github.com>
2023-12-01 12:03:30 -0600

    7- RestResource Personalizado

--- springboot-service-usuarios/src/main/java/com/backend/app/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/backend/app/usuarios/repository/UsuarioRepository.java
@@ -3,14 +3,18 @@ package com.backend.app.usuarios.repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+
 import com.backend.app.usuarios.entity.Usuario;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.repository.query.Param;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 	
 	// Select U From Usuario U Where U.username = ?1
-	public Usuario findByUsername(String username);
+	@RestResource(path = "buscar-username")
+	public Usuario findByUsername(@Param("nombre")String username);
 	
 	// Select U From Usuario U Where U.username = ?1 and U.email = ?2
 	public Usuario findByUsernameAndEmail(String username, String email);
