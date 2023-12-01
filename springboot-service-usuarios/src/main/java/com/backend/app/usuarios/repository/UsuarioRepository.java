bbe6121aaaa642009b21968bcb81e7d9defd8a0f
Azt3kCode <62321351+Azt3kCode@users.noreply.github.com>
2023-12-01 11:33:54 -0600

    5- RepositoryRestResource HATEOAS

--- springboot-service-usuarios/src/main/java/com/backend/app/usuarios/repository/UsuarioRepository.java
+++ springboot-service-usuarios/src/main/java/com/backend/app/usuarios/repository/UsuarioRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.backend.app.usuarios.entity.Usuario;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
 	
 	// Select U From Usuario U Where U.username = ?1
---
+++ springboot-service-usuarios/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email)
+VALUES ('alexis', '180902', 1, 'Alexis', 'Taco', 'alexis.taco@uanl.edu.mx')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email)
+VALUES ('antonella', '070802', 1, 'Antonella', 'Pizza', 'antonella.pizza@uanl.edu.mx')
